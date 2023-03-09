f0128ebb78aeb136f1380e17e8ace54d7772ae9e
Kristen Surrao <35614246+kmsurrao@users.noreply.github.com>
2023-03-09 09:57:20 -0500

    added MCILC code, option for 545 GHz, CMB blackbody SNR, plots for paper

--- planck_noise.py
+++ planck_noise.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 #Planck
-Nfreqs_Planck = 7
+Nfreqs_Planck = 8
 freqs_Planck = []
 freqs_Planck.append('030')
 freqs_Planck.append('044')
@@ -10,7 +10,8 @@ freqs_Planck.append('100')
 freqs_Planck.append('143')
 freqs_Planck.append('217')
 freqs_Planck.append('353')
-freqs_Planck_float = np.array([30.0, 44.0, 70.0, 100.0, 143.0, 217.0, 353.0])
+freqs_Planck.append('545')
+freqs_Planck_float = np.array([30.0, 44.0, 70.0, 100.0, 143.0, 217.0, 353.0, 545.0])
 # Planck noise
 noise_arr_Planck = np.zeros(Nfreqs_Planck)
 noise_arr_Planck[0] = 195.079975053 #uK-arcmin, from Table 7 (first column) of https://arxiv.org/pdf/1502.01585.pdf -- converted via sqrt(3224.4*(4*Pi*(180/Pi)^2*60^2/(12*1024^2)))
@@ -20,6 +21,7 @@ noise_arr_Planck[3] = 77.4 #uK-arcmin, from Table 6 of https://arxiv.org/pdf/150
 noise_arr_Planck[4] = 33.0
 noise_arr_Planck[5] = 46.8
 noise_arr_Planck[6] = 153.6
+noise_arr_Planck[7] = 0.78 * 1e-3 * 0.01723080316 * 1e6 * 60 #kJy/sr * deg --> converted to uK-arcmin
 # Planck noise -- pol.
 noise_arr_Planck_pol = np.zeros(Nfreqs_Planck)
 noise_arr_Planck_pol[0] = 276.262025938 #uK-arcmin, from Table 7 (first column) of https://arxiv.org/pdf/1502.01585.pdf -- converted via sqrt( sqrt(6467.8*6465.1) *(4*Pi*(180/Pi)^2*60^2/(12*1024^2)))
@@ -38,6 +40,8 @@ FWHM_arr_Planck[3] = 9.69 #arcmin, from Table 6 of https://arxiv.org/pdf/1502.01
 FWHM_arr_Planck[4] = 7.30
 FWHM_arr_Planck[5] = 5.02
 FWHM_arr_Planck[6] = 4.94
+FWHM_arr_Planck[7] = 4.83 #arcmin
+
 # convert to sigma in radians
 sigma_arr_Planck = FWHM_arr_Planck / np.sqrt(8. * np.log(2)) /60. * np.pi/180.
 # Planck noise power spectra
@@ -48,11 +52,11 @@ ell = np.arange(0,ell_max+1,delta_ell)
 PS_noise_Planck = np.zeros((Nfreqs_Planck,np.int(ell_max)+1))
 PS_noise_Planck_pol = np.zeros((Nfreqs_Planck,np.int(ell_max)+1))
 for i in range(Nfreqs_Planck):
-  PS_noise_Planck[i] = (noise_arr_Planck[i] * (1.0/60.0) * (np.pi/180.0))**2.0 * np.exp( ell*(ell+1)* sigma_arr_Planck[i]**2. ) #square to get the white-noise level -- see e.g. Eq. 2.32 (pg 15) of https://arxiv.org/pdf/1509.06770v4.pdf
-  PS_noise_Planck_pol[i] = (noise_arr_Planck_pol[i] * (1.0/60.0) * (np.pi/180.0))**2.0 * np.exp( ell*(ell+1)* sigma_arr_Planck[i]**2. ) #square to get the white-noise level -- see e.g. Eq. 2.32 (pg 15) of https://arxiv.org/pdf/1509.06770v4.pdf
-  # handle overflow due to high noise at high ell
-  PS_noise_Planck[i][(np.where(PS_noise_Planck[i] > MAX_NOISE))[0]] = MAX_NOISE
-  PS_noise_Planck_pol[i][(np.where(PS_noise_Planck_pol[i] > MAX_NOISE))[0]] = MAX_NOISE
+    PS_noise_Planck[i] = (noise_arr_Planck[i] * (1.0/60.0) * (np.pi/180.0))**2.0 * np.exp( ell*(ell+1)* sigma_arr_Planck[i]**2. ) #square to get the white-noise level -- see e.g. Eq. 2.32 (pg 15) of https://arxiv.org/pdf/1509.06770v4.pdf
+    #PS_noise_Planck_pol[i] = (noise_arr_Planck_pol[i] * (1.0/60.0) * (np.pi/180.0))**2.0 * np.exp( ell*(ell+1)* sigma_arr_Planck[i]**2. ) #square to get the white-noise level -- see e.g. Eq. 2.32 (pg 15) of https://arxiv.org/pdf/1509.06770v4.pdf
+    # handle overflow due to high noise at high ell
+    PS_noise_Planck[i][(np.where(PS_noise_Planck[i] > MAX_NOISE))[0]] = MAX_NOISE
+    #PS_noise_Planck_pol[i][(np.where(PS_noise_Planck_pol[i] > MAX_NOISE))[0]] = MAX_NOISE
 
 
 #added below
