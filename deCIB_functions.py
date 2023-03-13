d1fa9ea54c276fd08429003e025e65792aea562b
olakusiak <62485729+olakusiak@users.noreply.github.com>
2023-03-13 13:41:17 +0100

    rename folders, benchamark data added

--- deCIB_functions.py
+++ deCIB_functions.py
@@ -88,3 +88,47 @@ def make_flux_cut_list(cib_flux, nu_list):
             print(0)
             print("Flux cuts: ", cib_flux_list)
     return cib_flux_list
+
+
+def cib_spectral_response(freqs, Tdust_CIB,beta_CIB ): #input frequency in GHz
+    # from pyilc
+    # CIB = modified blackbody here
+    # N.B. overall amplitude is not meaningful here; output ILC map (if you tried to preserve this component) would not be in sensible units
+
+    TCMB = 2.726 #Kelvin
+    TCMB_uK = 2.726e6 #micro-Kelvin
+    hplanck=6.626068e-34 #MKS
+    kboltz=1.3806503e-23 #MKS
+    clight=299792458.0 #MKS
+
+    # function needed for Planck bandpass integration/conversion following approach in Sec. 3.2 of https://arxiv.org/pdf/1303.5070.pdf
+    # blackbody derivative
+    # units are 1e-26 Jy/sr/uK_CMB
+    def dBnudT(nu_ghz):
+        nu = 1.e9*np.asarray(nu_ghz)
+        X = hplanck*nu/(kboltz*TCMB)
+        return (2.*hplanck*nu**3.)/clight**2. * (np.exp(X))/(np.exp(X)-1.)**2. * X/TCMB_uK
+
+    # conversion from specific intensity to Delta T units (i.e., 1/dBdT|T_CMB)
+    #   i.e., from W/m^2/Hz/sr (1e-26 Jy/sr) --> uK_CMB
+    #   i.e., you would multiply a map in 1e-26 Jy/sr by this factor to get an output map in uK_CMB
+    def ItoDeltaT(nu_ghz):
+        return 1./dBnudT(nu_ghz)
+
+
+
+#     Tdust_CIB = 24.0       #CIB effective dust temperature [K] (Table 9 of http://www.aanda.org/articles/aa/pdf/2014/11/aa22093-13.pdf)
+#     beta_CIB = 1.2         #CIB modified blackbody spectral index (Table 9 of http://www.aanda.org/articles/aa/pdf/2014/11/aa22093-13.pdf ; Table 10 of that paper contains CIB monopoles)
+    nu0_CIB_ghz = 353.0    #CIB pivot frequency [GHz]
+    kT_e_keV = 5.0         #electron temperature for relativistic SZ evaluation [keV] (for reference, 5 keV is rough temperature for a 3x10^14 Msun/h cluster at z=0 (e.g., Arnaud+2005))
+    nu0_radio_ghz = 150.0  #radio pivot frequency [GHz]
+    beta_radio = -0.5      #radio power-law index
+
+    nu_ghz = freqs
+    nu = 1.e9*np.asarray(nu_ghz).astype(float)
+    X_CIB = hplanck*nu/(kboltz*Tdust_CIB)
+    nu0_CIB = nu0_CIB_ghz*1.e9
+    X0_CIB = hplanck*nu0_CIB/(kboltz*Tdust_CIB)
+    resp = (nu/nu0_CIB)**(3.0+(beta_CIB)) * ((np.exp(X0_CIB) - 1.0) / (np.exp(X_CIB) - 1.0)) * (ItoDeltaT(np.asarray(nu_ghz).astype(float))/ItoDeltaT(nu0_CIB_ghz))
+    #resp[np.where(nu_ghz == None)] = 0. #this case is appropriate for HI or other maps that contain no CMB-relevant signals (and also no CIB); they're assumed to be denoted by None in nu_ghz
+    return resp
